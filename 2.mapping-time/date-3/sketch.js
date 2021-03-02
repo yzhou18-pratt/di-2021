6b162b5960a3614c8d6687b2b994a6b22710d394
yzhou18-pratt <yzhou18-pratt@users.noreply.github.com>
2021-03-02 15:09:15 -0500

    date 3

--- 2.mapping-time/date-3/sketch.js
+++ 2.mapping-time/date-3/sketch.js
@@ -8,10 +8,34 @@ function setup() {
 function draw() {
   // check the clock for the current time and unpack some of its fields to generate a time-string
   var now = clock()
-
  
-  background(255)
+  colorMode(RGB)
+  textFont("Nunito") // ← check index.html to see how it was loaded from google-fonts
+  textSize(32) // make it big
+  textAlign(CENTER)
 
- 
+  background(242, 183, 5)
+
+  var cx = 250
+  var cy = 250
+
+  strokeWeight(15)
+  stroke(79, 178, 215)
+  noFill()
+  let w1 = map(now.progress.week, 0, 1, 0, 450)
+  ellipse(cx, cy, w1, w1)
+
+  strokeWeight(15)
+  stroke(242, 126, 211)
+  noFill()
+  let m1 = map(now.progress.month, 0, 1, 0, 450)
+  ellipse(cx, cy, m1, m1)
+
+  strokeWeight(15)
+  stroke(217, 103, 4, 150)
+  noFill()
+  let moo1 = map(now.progress.moon, 0, 1, 0, 450)
+  ellipse(cx, cy, moo1, moo1)
+
 
 }
